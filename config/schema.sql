155f67705d78f8a82c914a9de7956e67fd2fedb7
Luciano Rocha <rocha.lucho@gmail.com>
2014-05-10 19:18:31 -0300

    :)

--- config/schema.sql
+++ config/schema.sql
@@ -30,11 +30,7 @@ DROP TABLE IF EXISTS phones;
 CREATE TABLE phones (
   id INT NOT NULL auto_increment PRIMARY KEY,
   type VARCHAR(20) NOT NULL,
-<<<<<<< HEAD
   num INT NOT NULL,
-=======
-  cel_number INT NOT NULL PRIMARY KEY,
->>>>>>> 494dec83d5edad50ca4ecc8f6f59a234fb0add3d
   user_id int NOT NULL
 );
 
@@ -46,7 +42,6 @@ CREATE TABLE vehicles (
   id int NOT NULL auto_increment PRIMARY KEY,
   user_id INT NOT NULL,
   name VARCHAR(45) NOT NULL,
-<<<<<<< HEAD
   brand VARCHAR(45) NOT NULL,
   year INT(4) NOT NULL,
   type VARCHAR(20) NOT NULL,
@@ -55,52 +50,6 @@ CREATE TABLE vehicles (
   max_load INT,
   doors INT
 );
-
-=======
-  year INT(4) NOT NULL,
-  user_id INT NOT NULL
-);
-
-/**********************/
-/* Table for 'Car' */
-DROP TABLE IF EXISTS cars;
-CREATE TABLE cars (
-  id int NOT NULL PRIMARY KEY,
-  vehicle_id int NOT NULL,
-  max_capacity INT NOT NULL
-);
-
-/**********************/
-/* Table for 'Motorcycle' */
-DROP TABLE IF EXISTS motorcycles;
-CREATE TABLE motorcycles (
-  id int NOT NULL PRIMARY KEY,
-  vehicle_id int NOT NULL,
-  cilindrada INT NOT NULL
-);
-
-/**********************/
-/* Table for 'Truck' */
-DROP TABLE IF EXISTS trucks;
-CREATE TABLE trucks (
-  id int NOT NULL PRIMARY KEY,
-  vehicle_id int NOT NULL,
-  heigth INT NOT NULL,
-  max_capacity INT NOT NULL,
-  max_long INT NOT NULL
-);
-
-/**********************/
-/* Table for 'Other' */
-DROP TABLE IF EXISTS others;
-CREATE TABLE others (
-  id int NOT NULL PRIMARY KEY,
-  vehicle_id int NOT NULL,
-  max_capacity INT,
-  type VARCHAR(45) NOT NULL
-);
->>>>>>> 494dec83d5edad50ca4ecc8f6f59a234fb0add3d
-
 /**********************/
 /* Table for 'Post' */
 DROP TABLE IF EXISTS posts;
@@ -108,11 +57,7 @@ CREATE TABLE posts (
   id int NOT NULL auto_increment PRIMARY KEY,
   user_id INT NOT NULL,
   vehicle_id INT NOT NULL,
-<<<<<<< HEAD
   text VARCHAR(45) NOT NULL
-=======
-  user_id INT NOT NULL
->>>>>>> 494dec83d5edad50ca4ecc8f6f59a234fb0add3d
 );
 
 
@@ -122,12 +67,8 @@ DROP TABLE IF EXISTS rates;
 CREATE TABLE rates (
   id int NOT NULL auto_increment PRIMARY KEY,
   post_id INT NOT NULL,
-<<<<<<< HEAD
   user_id int NOT NULL,
   rate INT NOT NULL
-=======
-  user_id int NOT NULL
->>>>>>> 494dec83d5edad50ca4ecc8f6f59a234fb0add3d
 );
 
 
@@ -137,12 +78,8 @@ DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
   id int NOT NULL auto_increment PRIMARY KEY,
   post_id INT NOT NULL,
-<<<<<<< HEAD
   user_id int NOT NULL,
   question VARCHAR(80) NOT NULL
-=======
-  user_id int NOT NULL
->>>>>>> 494dec83d5edad50ca4ecc8f6f59a234fb0add3d
 );
 
 
@@ -152,10 +89,6 @@ DROP TABLE IF EXISTS answers;
 CREATE TABLE answers (
   id int NOT NULL auto_increment PRIMARY KEY,
   question_id INT NOT NULL,
-<<<<<<< HEAD
   user_id int NOT NULL,
   answer VARCHAR(80) NOT NULL
-=======
-  user_id int NOT NULL
->>>>>>> 494dec83d5edad50ca4ecc8f6f59a234fb0add3d
 );
