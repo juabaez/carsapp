cc552d62773629708c7401c9770a7bba277d83ae
Luciano Rocha <rocha.lucho@gmail.com>
2014-05-23 18:44:34 -0300

    Spark: First attempt.

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -1,13 +1,12 @@
 package com.unrc.app;
 
 import com.unrc.app.models.City;
-import org.javalite.activejdbc.Base;
-
 import com.unrc.app.models.User;
 import java.util.List;
-import com.unrc.app.models.Vehicle;
-import com.unrc.app.models.Car;
+import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.Model;
+import static org.javalite.test.jspec.JSpec.the;
+import spark.Spark;
 import static spark.Spark.*;
 
 /**
@@ -19,65 +18,30 @@ public class App
     public static void main(String[] args)
     {
         System.out.println("Hola mundo cruel!");
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "");
-        Base.openTransaction();
-        /********************************************************************/
-
-        City city = new City();
-        city.set("name", "Rio Cuarto", "state", "Cordoba", "country", "Argentina", "postcode", "5800");
-        city.saveIt();
+        /********************************************************************/   
+//            
+//        Spark.before(null);
+//        Spark.after(null);
         
-        User user = new User();
-        user
-            .firstName("Lucho")
-            .lastName("Rocha")
-            .email("rocha.lucho@gmail.com")
-            .pass("123456")
-            .address("Peru 725");
-        user.setParent(city);
-        user.saveIt();
-        
-        Car car = new Car();
-        car.set("name", "Ka", "brand", "Ford", "year", "2007", "passengers", "4");
-        car.setParent(user);
-        car.saveIt();
-        
-        
-        City city2 = new City();
-        city2.set("name", "Rio Tercero", "state", "Cordoba", "country", "Argentina", "postcode", "5800");
-        city2.saveIt();
-        
-        User user2 = new User();
-        user2.set("first_name", "John", "last_name", "Doe", "pass", "12345", "email", "example2222@email.com", "address", "Street One 123");
-        user2.setParent(city2);
-        user2.saveIt();
-        
-        Car car2 = new Car();
-        car2.set("name", "Ka222", "brand", "Ford222", "year", "2007", "passengers", "4");
-        car2.setParent(user2);
-        car2.saveIt();
-        
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        List<Vehicle> lista = Vehicle.filter("brand", "Ford");
-        System.out.println(lista.toString());
-        System.out.println(Vehicle.vehiclesFrom("Rio Tercero"));
-        
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        
-              get("/hello", (request, response) -> {
-         return "Hello World!";
+        get("/hello", (request, response) -> {
+            String ua = "lucho";
+            return ua;
       });
+        
+        get("/users", (request, response) -> {
+            Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/carsapp_development", "root", "");
+            String body = "<b><h2>Usuarios:</h2></b><br>";
+            for(User u : User.findAll()) {
+                body += "<font color=\"blue\">" + u.toString() + "\temail:" + u.email() +"</font><br>";
+            }
+            Base.close();
+            return body;
+      });
+        
+        
+        
+        
         /********************************************************************/
-        Base.rollbackTransaction();
-        Base.close();
     }
     
 }
--- src/main/java/com/unrc/app/models/User.java
+++ src/main/java/com/unrc/app/models/User.java
@@ -1,6 +1,7 @@
 package com.unrc.app.models;
 
 import java.util.List;
+import org.javalite.activejdbc.LazyList;
 import org.javalite.activejdbc.Model;
 
 public class User extends Model {
@@ -18,6 +19,10 @@ public class User extends Model {
               .pass(pass)
               .setParent(city);
       return user.saveIt();
+  }
+  
+  public static LazyList<User> findAll(){
+      return Model.findAll();
   }
   
   public User firstName(String s) {
@@ -51,7 +56,7 @@ public class User extends Model {
   }
   
   public String email() {   
-    return (this.getString("email"));
+    return this.getString("email");
   }
   
   public String address(){
