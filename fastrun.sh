db0f230392e1711756d5f8d6145982dc232886d8
Luciano Rocha <rocha.lucho@gmail.com>
2014-10-23 13:28:51 -0300

    Added "fastrun.sh"
            Added an additional script for run the project without cleaning and re-compiling.
            It only sets classpath and then launches main class.

---
+++ fastrun.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export CLASPATH=""
+for file in $(ls target/dependency); do export CLASSPATH=$CLASSPATH:target/dependency/$file; done
+export CLASSPATH=$CLASSPATH:target/classes
+
+echo "*******************  EXECUTING PROGRAM******************************************"
+java -cp $CLASSPATH -Dactivejdbc.log com.unrc.app.App 
